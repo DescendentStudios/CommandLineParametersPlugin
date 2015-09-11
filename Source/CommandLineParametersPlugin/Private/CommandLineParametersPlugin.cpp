6b997626a36ad1d7f4f98bb9bbda475aefe81ed4
Jason Spangler <jason.spangler@descendentstudios.com>
2015-09-11 15:03:32 -0500

    Basic game client and Web API integration, along with launcher support

---
+++ Source/CommandLineParametersPlugin/Private/CommandLineParametersPlugin.cpp
@@ -0,0 +1,26 @@
+// Some copyright should be here...
+
+#include "CommandLineParametersPluginPrivatePCH.h"
+
+
+
+#define LOCTEXT_NAMESPACE "FCommandLineParametersPluginModule"
+
+void FCommandLineParametersPluginModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	
+	
+}
+
+void FCommandLineParametersPluginModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+	
+	
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FCommandLineParametersPluginModule, CommandLineParametersPlugin)
